481c58f686d04fecbd432511cb27f0a626eba087
unknown <you@example.com>
2022-12-29 15:13:40 +0100

    first commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,61 +1,76 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-    const {  Fullname,State,email, City,
-        RestaurantName,
-        Country,
-       UploadRestaurantPhoto,
-        Address,
-       password,
-       PhoneNumber } = req.body;
-    try {
-        const user = await User.findOne({ email: email })
-        if (user) return res.status(400).json({ msg: 'User already exists' })
-        const newUser = new User({
-            email: email,
-            Fullname: Fullname,
-            State: State,
-             City: City,
-            RestaurantName: RestaurantName,
-            Country: Country,
-           UploadRestaurantPhoto: UploadRestaurantPhoto,
-            Address: Address,
-           password: password,
-           PhoneNumber: PhoneNumber,
-           leftoverAvailable: leftoverAvailable,
-        })
-        await newUser.save()
-        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET)
-        res.status(200).json({email: newUser.email, Fullname: newUser.Fullname,   State: newUser.State, City : newUser.City, RestaurantName: newUser.RestaurantName ,  Country: newUser.Country,  Address: newUser.Address, leftoverAvailable: newUser.leftoverAvailable, token: token})
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ msg: error.message })
-    }
-}
+  const {
+    Fullname,
+    State,
+    email,
+    City,
+    RestaurantName,
+    Country,
+    UploadRestaurantPhoto,
+    Address,
+    password,
+    PhoneNumber,
+    leftoverAvailable,
+  } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (user) return res.status(400).json({ msg: "User already exists" });
+    const newUser = new User({
+      email: email,
+      Fullname: Fullname,
+      State: State,
+      City: City,
+      RestaurantName: RestaurantName,
+      Country: Country,
+      UploadRestaurantPhoto: UploadRestaurantPhoto,
+      Address: Address,
+      password: password,
+      PhoneNumber: PhoneNumber,
+      leftoverAvailable: leftoverAvailable,
+    });
+    await newUser.save();
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .json({
+        email: newUser.email,
+        Fullname: newUser.Fullname,
+        State: newUser.State,
+        City: newUser.City,
+        RestaurantName: newUser.RestaurantName,
+        Country: newUser.Country,
+        Address: newUser.Address,
+        leftoverAvailable: newUser.leftoverAvailable,
+        token: token,
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: error.message });
+  }
+};
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email: email })
-        if (!user) return res.status(400).json({ msg: 'User does not exist' })
-        if (password !== user.password) return res.status(401).json({ msg: 'Incorrect password' })
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-        res.json({
-            token,
-            user: {
-                id: user._id,
-                email: user.email
-            }
-        })
-    } catch (error) {
-        return res.status(500).json({ msg: error.message })
-    }
-}
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+    if (password !== user.password)
+      return res.status(401).json({ msg: "Incorrect password" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
 
-
-
-
-module.exports = {register, login}
+module.exports = { register, login };
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { register, login } = require("../controllers/user");
 
-
 router.post("/register", register);
 router.post("/login", login);
 module.exports = router;
